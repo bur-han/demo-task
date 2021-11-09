65d52f713ada8c6bf5074293b034025fda6dc80f
burhaaan <burhanbabar54@gmail.com>
2021-11-09 19:30:14 +0500

    todos crud implementation

--- routes/todos.js
+++ routes/todos.js
@@ -14,9 +14,9 @@ router.get('/', async (req, res) => {
 })
 
 // // Getting One
-// router.get('/:id', getSubscriber, (req, res) => {
-//   res.json(res.subscriber)
-// })
+router.get('/:id', getTodo, (req, res) => {
+  res.json(res.todo)
+})
 
 // Creating one
 router.post('/', async (req, res) => {
@@ -32,44 +32,42 @@ router.post('/', async (req, res) => {
 })
 
 // // Updating One
-// router.patch('/:id', getSubscriber, async (req, res) => {
-//   if (req.body.name != null) {
-//     res.subscriber.name = req.body.name
-//   }
-//   if (req.body.subscribedToChannel != null) {
-//     res.subscriber.subscribedToChannel = req.body.subscribedToChannel
-//   }
-//   try {
-//     const updatedSubscriber = await res.subscriber.save()
-//     res.json(updatedSubscriber)
-//   } catch (err) {
-//     res.status(400).json({ message: err.message })
-//   }
-// })
+router.patch('/:id', getTodo, async (req, res) => {
+  if (req.body.name != null) {
+    res.todo.name = req.body.name
+  }
+  
+  try {
+    const updatedTodo = await res.todo.save()
+    res.json(updatedTodo)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
 
 // // Deleting One
-// router.delete('/:id', getSubscriber, async (req, res) => {
-//   try {
-//     await res.subscriber.remove()
-//     res.json({ message: 'Deleted Subscriber' })
-//   } catch (err) {
-//     res.status(500).json({ message: err.message })
-//   }
-// })
+router.delete('/:id', getTodo, async (req, res) => {
+  try {
+    await res.todo.remove()
+    res.json({ message: 'Deleted todo' })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
 
-// async function getSubscriber(req, res, next) {
-//   let subscriber
-//   try {
-//     subscriber = await Subscriber.findById(req.params.id)
-//     if (subscriber == null) {
-//       return res.status(404).json({ message: 'Cannot find subscriber' })
-//     }
-//   } catch (err) {
-//     return res.status(500).json({ message: err.message })
-//   }
+async function getTodo(req, res, next) {
+  let todo
+  try {
+    todo = await Todo.findById(req.params.id)
+    if (todo == null) {
+      return res.status(404).json({ message: 'Cannot find todo' })
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
 
-//   res.subscriber = subscriber
-//   next()
-// }
+  res.todo = todo
+  next()
+}
 
 module.exports = router
